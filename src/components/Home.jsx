a2af344132d3e157cd72dfeeca81847aa0457c99
Shaban Khan <90864408+playshaban@users.noreply.github.com>
2023-08-02 17:28:17 +0530

    Adding new home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import HomeS2 from "./home/HomeS2";
+import HomeS1 from "./home/HomeS1";
+import HomeS3 from "./home/HomeS3";
+import HomeS4 from "./home/HomeS4";
+import HomeS5 from "./home/HomeS5";
+import HomeS6 from "./home/HomeS6";
+import Pros from "./Pros";
+
+function Home() {
+  return (
+    <div className="Home">
+      <Navbar />
+      <HomeS1 />
+      <HomeS2 />
+      <HomeS3 />
+      <HomeS4 />
+      <HomeS5 />
+      <HomeS6 />
+      <Pros />
+    </div>
+  );
+}
+
+export default Home;
